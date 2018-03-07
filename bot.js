8f9ceb207e4cd71b470f9c0812ff5c7bcd70bc6e
GenericDM <miuiki200@gmail.com>
2018-03-07 12:41:04 -0800

    add bot framework

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
 //A bot for remot3.it on slack
+const { rtmClient, clientEvents } = require('@slack/client');
+const token = process.env.SLACK_TOKEN;
+const appData = {};
+const rtm = new rtmClient(token, {
+    dataStore: false,
+    useRtmConnect: true,
+});
+//Ready event
+rtm.on(clientEvents.RTM.AUTHENTICATED, (connectData) => {
+    appData.selfID = connectData.self.id;
+    console.log(`Ready as ${appData.selfID} on team ${connectData.team.id}`);
+});
+//End ready event
