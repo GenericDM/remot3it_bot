8b66b2a785014fe3b9183aca93e3a7aa396ebaec
GenericDM <miuiki200@gmail.com>
2018-03-07 13:13:44 -0800

    add remote.it API integration

--- bot.js
+++ bot.js
@@ -2,13 +2,33 @@
 const { rtmClient, clientEvents } = require('@slack/client');
 const token = process.env.SLACK_TOKEN;
 const appData = {};
-const rtm = new rtmClient(token, {
+var remoteitSettings = {
+    "async": true,
+    "crossdomain": true,
+    "url": "https://api.remot3.it/apv/v23.5/user/login",
+    "method": "POST",
+    "headers": {
+        "developerkey": "",
+        "content-type": "application/json",
+        "cache-control": "no-cache:"
+    },
+    "processData": false,
+    "data": "{ \"username\" : \"username\", \"password\" }"
+}
+const bot = new rtmClient(token, {
     dataStore: false,
     useRtmConnect: true,
 });
-//Ready event
-rtm.on(clientEvents.RTM.AUTHENTICATED, (connectData) => {
+const web = new WebClient(token)
+
+bot.on(clientEvents.rtm.authenticated, (connectData) => {
     appData.selfID = connectData.self.id;
     console.log(`Ready as ${appData.selfID} on team ${connectData.team.id}`);
 });
-//End ready event
+
+bot.on(clientEvents.RTM.RTM_CONNECTION,OPENED, () => {
+    console.log(`Ready`);
+});
+
+bot.start();
+
